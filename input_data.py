8e25eaf1c99e409de619fd92f0083a9ab2680f0f
Marcos Manhanes <66021601+manhanes@users.noreply.github.com>
2024-12-07 03:05:03 -0300

    Solucionar Algoritmos em Python

---
+++ input_data.py
@@ -0,0 +1,7 @@
+info1 = input("Digite a primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+unindo_informacao = info1 + " " + info2
+
+print("As informações digitadas foram:", unindo_informacao)
+
